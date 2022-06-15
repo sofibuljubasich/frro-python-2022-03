ccd91f53957791c8f9a6f19cfcda04bf1480f476
Manuel Ferraretto <manuferraretto86@gmail.com>
2022-06-15 16:16:32 -0300

    Resolución ejercicio_01

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class Socio(Base):
     """Implementar un modelo Socio a traves de Alchemy que cuente con los siguientes campos:
         - id_socio: entero (clave primaria, auto-incremental, unico)
@@ -15,5 +16,24 @@ class Socio(Base):
     """
     __tablename__ = 'socios'
 
-    # Completar
+    id = Column(Integer, primary_key=True)
+    dni = Column(Integer, unique=True, nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+
+    def __init__(self, dni, nombre, apellido):
+        self.id = id
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __repr__(self):
+        return f"DNI: {self.dni}\nName: {self.nombre}\nSurname: {self.apellido}\n"
+
+
+s1 = Socio(dni=11111, nombre='Jon', apellido='Snow')
+s2 = Socio(dni=11321, nombre='Albus', apellido='Dumbledore')
+
+print(s1)
+print(s2)
 
