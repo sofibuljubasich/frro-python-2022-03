2e2ed39915260cd4e1a6a6499e3b8ab4389383de
Sofi Buljubasich <sofiabuljuba@gmail.com>
2022-06-29 20:40:27 -0300

    Start ejercicio 2

--- practico_05/ejercicio_02.py
+++ practico_05/ejercicio_02.py
@@ -1,57 +1,124 @@
-"""Base de Datos - ORM"""
-
+from setuptools import find_namespace_packages
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from ejercicio_01 import Base, Socio
+from sqlalchemy.ext.declarative import declarative_base
 
 from typing import List, Optional
 
 class DatosSocio():
 
     def __init__(self):
-        pass # Completar
+        engine = create_engine('sqlite:///socios.db')
+        Base = declarative_base(engine)
+        Base.metadata.create_all(engine, tables=Socio.__tablename__)
+
+        db_session = sessionmaker(bind = engine)
+        self.session = db_session()
+
+            
 
     def buscar(self, id_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su id. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        #first()--> Returns None or the row
+        try:
+            socio = self.session.query(Socio).filter(Socio.id_socio == id_socio).first()
+        except Exception as e:
+            print("Error: ",e)
+        finally:
+            self.session.close()
+        return socio
 
     def buscar_dni(self, dni_socio: int) -> Optional[Socio]:
         """Devuelve la instancia del socio, dado su dni. Devuelve None si no 
         encuentra nada.
         """
-        pass # Completar
+        try:
+            socio = self.session.query(Socio).filter(Socio.dni == dni_socio).first()
+        except Exception as e:
+            print("Error: ",e)
+        finally:
+            self.session.close()
+    
+        return socio
         
     def todos(self) -> List[Socio]:
         """Devuelve listado de todos los socios en la base de datos."""
-        pass # Completar
+        try:
+            socios = self.session.query(Socio).all()
+        except Exception as e:
+            print("Error: ",e)
+        finally:
+            self.session.close()
+        return socios
 
     def borrar_todos(self) -> bool:
         """Borra todos los socios de la base de datos. Devuelve True si el 
         borrado fue exitoso.
         """
-        pass # Completar
+        try:
+            self.session.query(Socio).all().delete()
+            self.session.commit()
+            rta = True
+        except:
+            rta = False
+        finally:
+            self.session.close()
+        return rta
+
+
 
     def alta(self, socio: Socio) -> Socio:
         """Agrega un nuevo socio a la tabla y lo devuelve"""
-        pass # Completar
+        try:
 
+            self.session.add(socio)
+            self.session.commit()
+        except Exception as e:
+            print("Error en el alta: ",e)
+        finally:
+            self.session.close()
+
+        return socio
     def baja(self, id_socio: int) -> bool:
         """Borra el socio especificado por el id. Devuelve True si el borrado 
         fue exitoso.
         """
-        pass # Completar
+        try:
+            self.session.query(Socio).filter(Socio.id_socio == id_socio).delete()
+            rta = True
+        except:
+            self.session.rollback()
+            rta = False
+        finally:
+           
+            self.session.commit()
+            self.session.close()
+        return rta
 
     def modificacion(self, socio: Socio) -> Socio:
         """Guarda un socio con sus datos modificados. Devuelve el Socio 
         modificado.
         """
-        pass # Completar
+        try:
+            self.session.query(Socio).filter(Socio.id_socio == socio.id_socio).update({'dni': socio.dni,'nombre':socio.nombre,'apellido':socio.apellido})
+            self.session.commit()
+        except:
+            self.session.rollback()
+        finally:
+            self.session.close()
+
+        return socio
+        
     
     def contarSocios(self) -> int:
         """Devuelve el total de socios que existen en la tabla"""
-        pass # Completar
+        rows = self.session.query(Socio).count()
+        return rows
+
+
 
 
 
